48bf78a4a0d13c2a07dc80571b3aeb90be64bb40
LOUIS DE GONZAGUE NGEZAHAYO <gonzague013@gmail.com>
2026-02-10 23:52:18 +0200

    Update StudentRegistration.java

--- SpringBootRESTfulAPI/Question2_Student_Registration/src/main/java/com/example/StudentRegistration.java
+++ SpringBootRESTfulAPI/Question2_Student_Registration/src/main/java/com/example/StudentRegistration.java
@@ -1,1 +1,22 @@
+package com.example.controller;
+
+import com.example.model.Student;
+import com.example.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/register")
+public class StudentRegistrationController {
+
+    @Autowired
+    private StudentService studentService;
+
+    // Register a new student
+    @PostMapping
+    public Student registerStudent(@RequestBody Student student) {
+        return studentService.registerStudent(student);
+    }
+}
+
 
