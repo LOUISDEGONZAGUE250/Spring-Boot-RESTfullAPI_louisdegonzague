5de5e641cf2b8bc8c1087b96bf8a99a7d5e0c66e
LOUIS DE GONZAGUE NGEZAHAYO <gonzague013@gmail.com>
2026-02-10 17:53:03 +0200

    Remove StudentRegistration class and main method

--- SpringBootRESTfulAPI/Question2_Student_Registration/src/main/java/com/example/StudentRegistration.java
+++ SpringBootRESTfulAPI/Question2_Student_Registration/src/main/java/com/example/StudentRegistration.java
@@ -1,11 +1,1 @@
-package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class StudentRegistration {
-    public static void main(String[] args) {
-        SpringApplication.run(StudentRegistration.class, args);
-    }
-}
